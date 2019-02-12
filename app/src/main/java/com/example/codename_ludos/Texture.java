ebaaef4f4de75a50117f0626c32ff0db788830e4
SausageManZ <arostem@gmail.com>
2019-02-12 21:07:31 +0100

    tex

---
+++ app/src/main/java/com/example/codename_ludos/Texture.java
@@ -0,0 +1,17 @@
+package com.example.codename_ludos;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.nfc.Tag;
+import android.opengl.GLUtils;
+import android.util.Log;
+
+import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.opengles.GL11;
+import javax.microedition.khronos.opengles.GL11Ext;
+
+public class Texture
+{
+    private int texture[] = new int[1];
+    
+}
