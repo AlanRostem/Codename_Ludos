8c5c5a5a567215163c4703ca322c09d592cd4dfd
TheOneZalo <Benjamin.whittaker1999@gmail.com>
2019-02-22 16:21:47 +0100

    protected mPos in GameEntity
    Changed mPos, mVel and mAcc from private to protected

--- app/src/main/java/com/example/codename_ludos/Games/Eggrun/Eggrun.java
+++ app/src/main/java/com/example/codename_ludos/Games/Eggrun/Eggrun.java
@@ -1,7 +1,27 @@
 package com.example.codename_ludos.Games.Eggrun;
 
+import android.view.MotionEvent;
+
 import com.example.codename_ludos.ArcadeMachine.ArcadeGame;
 
 public class Eggrun extends ArcadeGame {
     public Eggrun(){ super("Eggrun"); }
+
+
+
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void onTouchEvent(MotionEvent event) {
+
+    }
 }
