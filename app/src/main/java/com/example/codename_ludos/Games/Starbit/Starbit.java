6440d2e079ee4b02c8a89696d5d23b9d37d50d1e
TheOneZalo <Benjamin.whittaker1999@gmail.com>
2019-02-22 17:17:38 +0100

    Update Starbit.java

--- app/src/main/java/com/example/codename_ludos/Games/Starbit/Starbit.java
+++ app/src/main/java/com/example/codename_ludos/Games/Starbit/Starbit.java
@@ -6,4 +6,12 @@ public class Starbit extends ArcadeGame {
     public Starbit() {
         super("Starbit");
     }
+
+    private Player player = new Player();
+
+    @Override
+    public void setup() {
+        spawnEntuty(player);
+    }
+
 }
